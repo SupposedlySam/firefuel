0a6da592ca139220087ebfb2aa5fdc71deca0318
Jonah Walker <supposedlysam@gmail.com>
2021-08-24 16:26:30 -0600

    test: UIDSerializer

---
+++ test/src/data/models/uid/uid_serializer_test.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:firefuel/src/data/models/uid/uid_model.dart';
+import 'package:firefuel/src/data/models/uid/uid_serializer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const uidValue = 'someTestValue',
+      rawJson = """
+    {
+      "${UID.fieldOriginal}" : "$uidValue"
+    }
+    """;
+
+  test('#fromJson should convert correctly', () {
+    final uid = UIDSerializer.fromJson(json.decode(rawJson));
+
+    expect(uid, isA<UID>());
+  });
+
+  test('.toMap should convert correctly', () {
+    final instance = UIDSerializer(uidValue);
+    final result = UIDSerializer.toMap(instance);
+
+    expect(result, json.decode(rawJson));
+  });
+}
