fd6f0e04ddf021f6378fa00fb8853cd923dfdfc9
Jonah Walker <supposedlysam@gmail.com>
2021-08-24 16:23:18 -0600

    test: DocumentIdSerializer

--- test/src/data/models/document_id/document_id_serializer_test.dart
+++ test/src/data/models/document_id/document_id_serializer_test.dart
@@ -1,5 +1,27 @@
+import 'dart:convert';
+
+import 'package:firefuel/src/data/models/document_id/document_id_model.dart';
+import 'package:firefuel/src/data/models/document_id/document_id_serializer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('', () {});
+  const docIdValue = 'someTestValue',
+      rawJson = """
+    {
+      "${DocumentId.fieldDocId}" : "$docIdValue"
+    }
+    """;
+
+  test('#fromJson should convert correctly', () {
+    final docId = DocumentIdSerializer.fromJson(json.decode(rawJson));
+
+    expect(docId, isA<DocumentId>());
+  });
+
+  test('.toMap should convert correctly', () {
+    final instance = DocumentIdSerializer(docIdValue);
+    final result = DocumentIdSerializer.toMap(instance);
+
+    expect(result, json.decode(rawJson));
+  });
 }
