75cd557d99bf418bf29cc23a32d4daeedc7dfa80
Jonah Walker <supposedlysam@gmail.com>
2021-08-25 11:15:04 -0600

    feat: update package interface

--- lib/firefuel.dart
+++ lib/firefuel.dart
@@ -1,3 +1,7 @@
 library firefuel;
 
-export 'package:firefuel/src/data/data.dart';
+export 'package:dartz/dartz.dart' show Either, Right, Left, left;
+export 'package:firefuel/src/firefuel_collection.dart';
+export 'package:firefuel/src/firefuel_repository.dart';
+export 'package:firefuel/src/firefuel_fetch_mixin.dart';
+export 'package:firefuel/src/firefuel_failure.dart';
