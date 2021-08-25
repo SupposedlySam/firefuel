f4dd6a9fff04daa0f43418d60695c4baad053c95
Jonah Walker <supposedlysam@gmail.com>
2021-08-25 12:34:58 -0600

    refactor: move props to Failure

---
+++ packages/firefuel_core/lib/src/failure.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:equatable/equatable.dart';
+import 'package:stack_trace/stack_trace.dart';
+
+abstract class Failure extends Equatable {
+  final Object error;
+  final Chain stackTrace;
+
+  Failure(this.error, {required this.stackTrace});
+
+  @override
+  List<Object> get props => [error, stackTrace];
+
+  @override
+  String toString() {
+    return '${error.toString()}\n\n${stackTrace.terse}';
+  }
+}
+
+abstract class FirefuelFailure extends Failure {
+  FirefuelFailure({required Object error, required Chain stackTrace})
+      : super(error, stackTrace: stackTrace) {
+    final isTesting = Platform.environment.containsKey('FLUTTER_TEST');
+
+    if (!isTesting) {
+      print(toString());
+    }
+  }
+}
