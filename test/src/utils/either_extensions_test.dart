5893b18bd501d3eb27c23d992ac0f9620db541dd
Morgan <mrgnhnt96@gmail.com>
2021-08-27 12:44:22 -0500

    test: EitherExtension.getRight()

--- test/src/utils/either_extensions_test.dart
+++ test/src/utils/either_extensions_test.dart
@@ -5,8 +5,28 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../utils/expected_failure.dart';
 
 void main() {
+  group('#getRight', () {
+    test('should return Right when successful', () {
+      final Either<Failure, String> result = Right('testValue');
+
+      final right = result.getRight();
+
+      expect(right, isA<String>());
+    });
+
+    test('should throw $MissingValueException when Right was not successful',
+        () {
+      final Either<Failure, String> result = Left(ExpectedFailure());
+
+      expect(
+        () => result.getRight(),
+        throwsA(isA<MissingValueException>()),
+      );
+    });
+  });
+
   group('#getOrElseNull', () {
-    test('should return a nullable T', () {
+    test('should return a nullable Right', () {
       final Either<Failure, String> right = Right('testValue');
 
       final result = right.getOrElseNull();
@@ -14,7 +34,7 @@ void main() {
       expect(result, isA<String?>());
     });
 
-    test('should return right when right is provided', () {
+    test('should return Right value when Right is provided', () {
       final success = 'testValue';
       final Either<Failure, String> right = Right(success);
 
@@ -23,39 +43,39 @@ void main() {
       expect(result, success);
     });
 
-    test('should return null when left is provided', () {
-      final Either<Failure, String> left = Left(ExpectedFailure());
+    test('should return null when not successful', () {
+      final Either<Failure, String> result = Left(ExpectedFailure());
 
-      final result = left.getOrElseNull();
+      final right = result.getOrElseNull();
 
-      expect(result, isNull);
+      expect(right, isNull);
     });
   });
 
   group('#getLeftOrElseNull', () {
     test('should return a nullable $Failure', () {
-      final Either<Failure, String> left = Left(ExpectedFailure());
+      final Either<Failure, String> result = Left(ExpectedFailure());
 
-      final result = left.getLeftOrElseNull();
+      final left = result.getLeftOrElseNull();
 
-      expect(result, isA<Failure?>());
+      expect(left, isA<Failure?>());
     });
 
-    test('should return left when left is provided', () {
+    test('should return Left when $Failure exists', () {
       final failure = ExpectedFailure();
-      final Either<Failure, String> left = Left(failure);
+      final Either<Failure, String> result = Left(failure);
 
-      final result = left.getLeftOrElseNull();
+      final left = result.getLeftOrElseNull();
 
-      expect(result, failure);
+      expect(left, failure);
     });
 
-    test('should return null when right is provided', () {
-      final Either<Failure, String> right = Right('testValue');
+    test('should return null when $Failure does not exist', () {
+      final Either<Failure, String> result = Right('testValue');
 
-      final result = right.getLeftOrElseNull();
+      final left = result.getLeftOrElseNull();
 
-      expect(result, isNull);
+      expect(left, isNull);
     });
   });
 }
