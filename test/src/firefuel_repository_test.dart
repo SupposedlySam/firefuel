2e8cc7ffac3d6fac337d4af3fdeb830fdc53fe7d
Jonah Walker <supposedlysam@gmail.com>
2021-08-30 14:52:38 -0600

    test: stub repository groups

---
+++ test/src/firefuel_repository_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('#create', () {}, skip: true);
+
+  group('#read', () {}, skip: true);
+  group('#readAsStream', () {}, skip: true);
+  group('#update', () {}, skip: true);
+  group('#delete', () {}, skip: true);
+}
