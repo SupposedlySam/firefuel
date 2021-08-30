044e953486a944a16406eb5a41ce4ed87f59660f
Jonah Walker <supposedlysam@gmail.com>
2021-08-30 15:18:18 -0600

    fix: add fallback value for Mocktail
    https://pub.dev/packages/mocktail#how-it-works

--- test/mocks/mock_collection.dart
+++ test/mocks/mock_collection.dart
@@ -4,11 +4,15 @@ import 'package:firefuel/firefuel.dart';
 import 'package:firefuel/src/collection.dart';
 import 'package:mocktail/mocktail.dart';
 
+import '../utils/test_user.dart';
+
 class MockCollection<T extends Serializable> extends Mock
     implements Collection<T> {}
 
 extension MockCollectionX<T extends Serializable> on MockCollection<T> {
   void initialize({DocumentId? createReturn}) {
+    registerFallbackValue<Serializable>(TestUser('fallbackValue'));
+
     if (createReturn != null) {
       when(
         () => create(docId: any(named: 'docId'), value: any(named: 'value')),
