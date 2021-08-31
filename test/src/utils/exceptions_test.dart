a658e0a8ebd23271aceaa000622b818011bd8ee2
Jonah Walker <supposedlysam@gmail.com>
2021-08-31 11:43:50 -0600

    test: MissingValueException

---
+++ test/src/utils/exceptions_test.dart
@@ -0,0 +1,12 @@
+import 'package:firefuel/firefuel.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('$MissingValueException', () {
+    test('should output Type when exception occurs', () {
+      final exception = MissingValueException(String);
+
+      expect(exception.toString(), 'MissingValueException:String');
+    });
+  });
+}
