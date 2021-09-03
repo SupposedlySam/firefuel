a84e48620a922d9e991b974747d79e14567bf66a
Morgan <mrgnhnt96@gmail.com>
2021-09-03 14:35:00 -0500

    chore: Add tests for Firefuel.firestore

---
+++ packages/firefuel/test/src/firefuel_test.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firefuel/firefuel.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('$Firefuel', () {
+    group('#firestore', () {
+      test('should throw an exception when not initialized', () {
+        expect(() => Firefuel.firestore, throwsA(isA<AssertionError>()));
+      });
+
+      test('should return a $FirebaseFirestore instance when initialized', () {
+        Firefuel.initialize(FakeFirebaseFirestore());
+
+        expect(Firefuel.firestore, isA<FirebaseFirestore>());
+      });
+    });
+  });
+}
