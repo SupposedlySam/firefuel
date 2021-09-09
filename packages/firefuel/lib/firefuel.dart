d1d453cc476e7231f2c97d53fa032ca7368bd7cd
Jonah Walker <supposedlysam@gmail.com>
2021-09-08 18:40:49 -0600

    feat: expose rules for package

--- packages/firefuel/lib/firefuel.dart
+++ packages/firefuel/lib/firefuel.dart
@@ -6,6 +6,7 @@ export 'package:firefuel/src/firefuel_collection.dart';
 export 'package:firefuel/src/firefuel_failure.dart';
 export 'package:firefuel/src/firefuel_fetch_mixin.dart';
 export 'package:firefuel/src/firefuel_repository.dart';
+export 'package:firefuel/src/rules.dart';
 export 'package:firefuel/src/utils/either_extensions.dart';
 export 'package:firefuel/src/utils/exceptions.dart';
 export 'package:firefuel_core/firefuel_core.dart';
