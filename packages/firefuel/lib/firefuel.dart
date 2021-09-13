c7ee0b86c8fa8b9cd60662bab1169a2bd522b76e
Jonah Walker <supposedlysam@gmail.com>
2021-09-13 17:28:18 -0600

    feat: export collection, repository, clause, and firestore subset

--- packages/firefuel/lib/firefuel.dart
+++ packages/firefuel/lib/firefuel.dart
@@ -1,12 +1,26 @@
 library firefuel;
 
-export 'package:dartz/dartz.dart' show Either, Right, Left, left;
+export 'package:cloud_firestore/cloud_firestore.dart'
+    show
+        CollectionReference,
+        DocumentSnapshot,
+        FieldPath,
+        Query,
+        QuerySnapshot,
+        QueryDocumentSnapshot,
+        SetOptions,
+        SnapshotOptions;
+export 'package:dartz/dartz.dart' show Either, Left, left, Right, right;
+export 'package:firefuel/src/clause.dart';
+export 'package:firefuel/src/collection.dart';
+export 'package:firefuel/src/repository.dart';
 export 'package:firefuel/src/firefuel.dart';
 export 'package:firefuel/src/firefuel_collection.dart';
 export 'package:firefuel/src/firefuel_failure.dart';
 export 'package:firefuel/src/firefuel_fetch_mixin.dart';
 export 'package:firefuel/src/firefuel_repository.dart';
 export 'package:firefuel/src/rules.dart';
+export 'package:firefuel/src/snapshot_conversion_mixin.dart';
 export 'package:firefuel/src/utils/either_extensions.dart';
 export 'package:firefuel/src/utils/exceptions.dart';
 export 'package:firefuel_core/firefuel_core.dart';
---
+++ packages/firefuel/lib/src/clause.dart
@@ -0,0 +1,29 @@
+class Clause {
+  final Object field;
+
+  final Object? isEqualTo;
+  final Object? isNotEqualTo;
+  final Object? isLessThan;
+  final Object? isLessThanOrEqualTo;
+  final Object? isGreaterThan;
+  final Object? isGreaterThanOrEqualTo;
+  final Object? arrayContains;
+  final List<Object?>? arrayContainsAny;
+  final List<Object?>? whereIn;
+  final List<Object?>? whereNotIn;
+  final bool? isNull;
+  Clause(
+    this.field, {
+    this.isEqualTo,
+    this.isNotEqualTo,
+    this.isLessThan,
+    this.isLessThanOrEqualTo,
+    this.isGreaterThan,
+    this.isGreaterThanOrEqualTo,
+    this.arrayContains,
+    this.arrayContainsAny,
+    this.whereIn,
+    this.whereNotIn,
+    this.isNull,
+  });
+}
