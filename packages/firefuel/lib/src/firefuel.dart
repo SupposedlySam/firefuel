994d5ff32f4340cfc2a2b49f73d5ce11c89d3ce9
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-22 13:27:27 -0500

    feat: Add collection prefix as optional param to #initialize

--- packages/firefuel/lib/src/firefuel.dart
+++ packages/firefuel/lib/src/firefuel.dart
@@ -10,12 +10,29 @@ class Firefuel {
 
   static FirebaseFirestore? _firestore;
 
+  static String get collectionPrefix {
+    if (_collectionPrefix == null) return '';
+    return '$_collectionPrefix-';
+  }
+
+  static String? _collectionPrefix;
+
   /// Initializes Firefuel with instance of [FirebaseFirestore]
   ///
   /// This method must be called before any other method of Firefuel
   ///
   /// All collections will reference [firestore]
-  static void initialize(FirebaseFirestore firestore) {
+  ///
+  /// ---
+  ///
+  /// [collectionPrefix] is optional and will be prepended to all collection names
+  ///
+  /// Useful for separating collections between app flavors / environments
+  static void initialize(
+    FirebaseFirestore firestore, {
+    String? collectionPrefix,
+  }) {
     _firestore = firestore;
+    _collectionPrefix = collectionPrefix;
   }
 }
