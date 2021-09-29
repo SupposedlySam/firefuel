87ba7e5c2ca13dca5154825ffdc380b9dedea546
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-29 16:55:53 -0500

    feat: Add firefuel initialize

--- packages/firefuel/examples/flutter_counter/lib/main.dart
+++ packages/firefuel/examples/flutter_counter/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firefuel/firefuel.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'counter_observer.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  // await Firebase.initializeApp();
+
+  // Used to mock Firestore
+  final firestoreInstance = FakeFirebaseFirestore();
+  Firefuel.initialize(firestoreInstance);
+
   Bloc.observer = CounterObserver();
   runApp(const CounterApp());
 }
