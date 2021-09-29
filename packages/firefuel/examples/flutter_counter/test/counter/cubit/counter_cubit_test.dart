8b41e263f8df04d33b702da3f0340b7672561271
Jonah Walker <supposedlysam@gmail.com>
2021-09-29 16:57:27 -0600

    test(fix): CounterCubit

--- packages/firefuel/examples/flutter_counter/test/counter/cubit/counter_cubit_test.dart
+++ packages/firefuel/examples/flutter_counter/test/counter/cubit/counter_cubit_test.dart
@@ -6,44 +6,45 @@ import 'package:flutter_counter/counter/data/collection/counter_collection.dart'
 import 'package:flutter_counter/counter/data/domain/counter_model.dart';
 import 'package:flutter_counter/counter/data/repo/counter_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 
 void main() {
-  setUpAll(() {
+  late CounterCubit counterCubit;
+
+  setUp(() {
     Firefuel.initialize(FakeFirebaseFirestore());
+    final counterRepository =
+        CounterRepository(collection: CounterCollection());
+    counterCubit = CounterCubit(counterRepo: counterRepository);
   });
 
   group('CounterCubit', () {
     test('initial state is 0', () {
-      expect(
-        CounterCubit(counterRepo: TestCounterRepository()).state,
-        const TestCounter(0),
-      );
+      expect(counterCubit.state, const TestCounter(0));
     });
 
     group('increment', () {
       blocTest<CounterCubit, Counter>(
         'emits [1] when state is 0',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
-        act: (cubit) => cubit.increment(),
-        wait: const Duration(milliseconds: 300),
+        build: () => counterCubit,
+        act: (cubit) async => await cubit.increment(),
         expect: () => const <Counter>[TestCounter(1)],
       );
 
       blocTest<CounterCubit, Counter>(
         'emits [1, 2] when state is 0 and invoked twice',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
-        act: (cubit) => cubit
-          ..increment()
-          ..increment(),
+        build: () => counterCubit,
+        act: (cubit) async {
+          await cubit.increment();
+          await cubit.increment();
+        },
         expect: () => const <Counter>[TestCounter(1), TestCounter(2)],
       );
 
       blocTest<CounterCubit, Counter>(
         'emits [42] when state is 41',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        build: () => counterCubit,
         seed: () => const TestCounter(41),
-        act: (cubit) => cubit.increment(),
+        act: (cubit) async => await cubit.increment(),
         expect: () => const <Counter>[TestCounter(42)],
       );
     });
@@ -51,25 +52,26 @@ void main() {
     group('decrement', () {
       blocTest<CounterCubit, Counter>(
         'emits [-1] when state is 0',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        build: () => counterCubit,
         act: (cubit) async => await cubit.decrement(),
         expect: () => const <Counter>[TestCounter(-1)],
       );
 
       blocTest<CounterCubit, Counter>(
         'emits [-1, -2] when state is 0 and invoked twice',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
-        act: (cubit) => cubit
-          ..decrement()
-          ..decrement(),
+        build: () => counterCubit,
+        act: (cubit) async {
+          await cubit.decrement();
+          await cubit.decrement();
+        },
         expect: () => const <Counter>[TestCounter(-1), TestCounter(-2)],
       );
 
       blocTest<CounterCubit, Counter>(
         'emits [42] when state is 43',
-        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        build: () => counterCubit,
         seed: () => const Counter(id: 'count', value: 43),
-        act: (cubit) => cubit.decrement(),
+        act: (cubit) async => await cubit.decrement(),
         expect: () => const <Counter>[TestCounter(42)],
       );
     });
@@ -79,9 +81,3 @@ void main() {
 class TestCounter extends Counter {
   const TestCounter(int value) : super(id: 'count', value: value);
 }
-
-class TestCounterRepository extends CounterRepository {
-  TestCounterRepository() : super(collection: TestCollection());
-}
-
-class TestCollection extends Mock implements CounterCollection {}
