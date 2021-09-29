7c4a44fbf8d89efe0fdd5aeb6aed7fb86538109f
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-29 15:25:49 -0500

    feat: create example from bloc

---
+++ packages/firefuel/examples/flutter_counter/lib/counter_observer.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observes all state changes.
+/// {@endtemplate}
+class CounterObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+}
