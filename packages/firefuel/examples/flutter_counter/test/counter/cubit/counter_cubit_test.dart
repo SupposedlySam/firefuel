604d5a2d8feed5c805c58cbbef8581ba04ff1476
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-29 16:56:32 -0500

    wip: Update tests to compile (not passing)

--- packages/firefuel/examples/flutter_counter/test/counter/cubit/counter_cubit_test.dart
+++ packages/firefuel/examples/flutter_counter/test/counter/cubit/counter_cubit_test.dart
@@ -1,63 +1,87 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firefuel/firefuel.dart';
 import 'package:flutter_counter/counter/counter.dart';
+import 'package:flutter_counter/counter/data/collection/counter_collection.dart';
+import 'package:flutter_counter/counter/data/domain/counter_model.dart';
+import 'package:flutter_counter/counter/data/repo/counter_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 void main() {
+  setUpAll(() {
+    Firefuel.initialize(FakeFirebaseFirestore());
+  });
+
   group('CounterCubit', () {
     test('initial state is 0', () {
-      expect(CounterCubit().state, 0);
+      expect(
+        CounterCubit(counterRepo: TestCounterRepository()).state,
+        const TestCounter(0),
+      );
     });
 
     group('increment', () {
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [1] when state is 0',
-        build: () => CounterCubit(),
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
         act: (cubit) => cubit.increment(),
-        expect: () => const <int>[1],
+        wait: const Duration(milliseconds: 300),
+        expect: () => const <Counter>[TestCounter(1)],
       );
 
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [1, 2] when state is 0 and invoked twice',
-        build: () => CounterCubit(),
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
         act: (cubit) => cubit
           ..increment()
           ..increment(),
-        expect: () => const <int>[1, 2],
+        expect: () => const <Counter>[TestCounter(1), TestCounter(2)],
       );
 
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [42] when state is 41',
-        build: () => CounterCubit(),
-        seed: () => 41,
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        seed: () => const TestCounter(41),
         act: (cubit) => cubit.increment(),
-        expect: () => const <int>[42],
+        expect: () => const <Counter>[TestCounter(42)],
       );
     });
 
     group('decrement', () {
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [-1] when state is 0',
-        build: () => CounterCubit(),
-        act: (cubit) => cubit.decrement(),
-        expect: () => const <int>[-1],
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        act: (cubit) async => await cubit.decrement(),
+        expect: () => const <Counter>[TestCounter(-1)],
       );
 
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [-1, -2] when state is 0 and invoked twice',
-        build: () => CounterCubit(),
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
         act: (cubit) => cubit
           ..decrement()
           ..decrement(),
-        expect: () => const <int>[-1, -2],
+        expect: () => const <Counter>[TestCounter(-1), TestCounter(-2)],
       );
 
-      blocTest<CounterCubit, int>(
+      blocTest<CounterCubit, Counter>(
         'emits [42] when state is 43',
-        build: () => CounterCubit(),
-        seed: () => 43,
+        build: () => CounterCubit(counterRepo: TestCounterRepository()),
+        seed: () => const Counter(id: 'count', value: 43),
         act: (cubit) => cubit.decrement(),
-        expect: () => const <int>[42],
+        expect: () => const <Counter>[TestCounter(42)],
       );
     });
   });
 }
+
+class TestCounter extends Counter {
+  const TestCounter(int value) : super(id: 'count', value: value);
+}
+
+class TestCounterRepository extends CounterRepository {
+  TestCounterRepository() : super(collection: TestCollection());
+}
+
+class TestCollection extends Mock implements CounterCollection {}
