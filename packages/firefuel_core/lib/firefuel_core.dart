5749e4912f6556587cb4d078c16eb8e1ad886756
Jonah Walker <supposedlysam@gmail.com>
2021-09-30 10:26:04 -0600

    refactor!: remove UID from project

--- packages/firefuel_core/lib/firefuel_core.dart
+++ packages/firefuel_core/lib/firefuel_core.dart
@@ -2,5 +2,5 @@ library firefuel_core;
 
 export 'package:firefuel_core/src/failure.dart';
 export 'package:firefuel_core/src/exceptions.dart';
-export 'package:firefuel_core/src/models/models.dart';
+export 'package:firefuel_core/src/models/document_id/document_id_model.dart';
 export 'package:firefuel_core/src/serializable.dart';
