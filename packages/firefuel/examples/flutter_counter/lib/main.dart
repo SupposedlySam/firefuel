bcd14b72070f473bfaf08592d7c7dfc82225ec62
Jonah Walker <supposedlysam@gmail.com>
2021-09-30 09:27:46 -0600

    feat: initialize firebase in main

--- packages/firefuel/examples/flutter_counter/lib/main.dart
+++ packages/firefuel/examples/flutter_counter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firefuel/firefuel.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,12 @@ import 'counter_observer.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final firestoreInstance = FakeFirebaseFirestore();
+
   // Initialize Firebase
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   // Used to mock Firestore
-  final firestoreInstance = FakeFirebaseFirestore();
   Firefuel.initialize(firestoreInstance);
 
   Bloc.observer = CounterObserver();
