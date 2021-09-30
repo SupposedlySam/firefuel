43e8ec8a6e1817e06ec59875064c3c256e209518
Jonah Walker <supposedlysam@gmail.com>
2021-09-30 10:05:01 -0600

    test: counter_page_test: add fake firestore

--- packages/firefuel/examples/flutter_counter/test/counter/view/counter_page_test.dart
+++ packages/firefuel/examples/flutter_counter/test/counter/view/counter_page_test.dart
@@ -1,13 +1,15 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firefuel/firefuel.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_counter/counter/counter.dart';
 import 'package:flutter_counter/counter/view/counter_view.dart';
 
 void main() {
+  setUp(() => Firefuel.initialize(FakeFirebaseFirestore()));
+
   group('CounterPage', () {
     testWidgets('renders CounterView', (tester) async {
       await tester.pumpWidget(MaterialApp(home: CounterPage()));
