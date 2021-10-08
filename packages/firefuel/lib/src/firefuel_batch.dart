a8a0b8647faf6e521484c8f7f792fee02ee6052c
Morgan Hunt <mrgnhnt96@gmail.com>
2021-10-08 16:12:14 -0500

    feat: Add methods for batch

--- packages/firefuel/lib/src/firefuel_batch.dart
+++ packages/firefuel/lib/src/firefuel_batch.dart
@@ -9,41 +9,46 @@ class FirefuelBatch<T extends Serializable> extends Batch<T> {
   FirefuelBatch(this.collection) : super(collection);
 
   @override
-  Future<int?> create(T value) {
-    // TODO: implement create
-    throw UnimplementedError();
-  }
+  Future<int?> create(T value) => transact((batch) {
+        batch.set(collection.ref.doc(), value);
+      });
 
   @override
-  Future<int?> createById({required T value, required DocumentId docId}) {
-    // TODO: implement createById
-    throw UnimplementedError();
-  }
+  Future<int?> createById({required T value, required DocumentId docId}) =>
+      transact((batch) {
+        batch.set(collection.ref.doc(docId.docId), value);
+      });
 
   @override
-  Future<int?> delete(DocumentId docId) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
+  Future<int?> delete(DocumentId docId) => transact((batch) {
+        batch.delete(collection.ref.doc(docId.docId));
+      });
 
   @override
-  Future<int?> replace({required DocumentId docId, required T value}) {
-    // TODO: implement replace
-    throw UnimplementedError();
-  }
+  Future<int?> replace({required DocumentId docId, required T value}) =>
+      transact((batch) async {
+        final existingDoc = await collection.read(docId);
+
+        if (existingDoc == null) return null;
+
+        batch.set(collection.ref.doc(docId.docId), value);
+      });
 
   @override
-  Future<int?> replaceFields(
-      {required DocumentId docId,
-      required T value,
-      required List<String> fieldPaths}) {
-    // TODO: implement replaceFields
-    throw UnimplementedError();
-  }
+  Future<int?> replaceFields({
+    required DocumentId docId,
+    required T value,
+    required List<String> fieldPaths,
+  }) =>
+      transact((batch) {
+        final replacement = value.getReplacement(fieldPaths);
+
+        batch.update(collection.ref.doc(docId.docId), replacement);
+      });
 
   @override
-  Future<int?> update({required DocumentId docId, required T value}) {
-    // TODO: implement update
-    throw UnimplementedError();
-  }
+  Future<int?> update({required DocumentId docId, required T value}) =>
+      transact((batch) {
+        batch.update(collection.ref.doc(docId.docId), value.toJson());
+      });
 }
