a79f653f1529315cd2590222e56e5b6e712e08e0
Jonah Walker <supposedlysam@gmail.com>
2022-09-19 10:27:44 -0600

    feat: provide global isOnline property

--- packages/firefuel/lib/src/firefuel.dart
+++ packages/firefuel/lib/src/firefuel.dart
@@ -15,6 +15,32 @@ class Firefuel {
 
   static String? _env;
 
+  /// Allows for all functions to write to Firestore without "await"ing their
+  /// results.
+  ///
+  /// Offline functionality is limited with `cloud_firestore` (which firefuel
+  /// wraps) and had a couple of caveats.
+  ///
+  /// 1. All offline updates are stored in a queue in memory. This means any
+  /// actions (reads/writes) taken by the user when offline will only be kept
+  /// around as long as the app is running in the foreground or background. Once
+  /// the app is killed, all unsynced data is lost.
+  /// 2. You can only perform actions on collections and documents that have
+  /// been previously cached in memory. For example, imagine you start your app
+  /// without internet access, at this point, `cloud_firestore` hasn't been able
+  /// to pull down any data, so your cached database is empty. You'll only be
+  /// able to create new collections>documents and get/listen to those created
+  /// while offline. The best use case for this functionality is when your user
+  /// has intermittent internet access. That way the cache has all the values
+  /// your user is expecting when they go to interact with the app. A second use
+  /// case could be when you want to give your user a "preview" of the app
+  /// before ever making real updates to your database. Just keep the following
+  /// point in mind.
+  /// 3. Offline queries are SLOOOW, local data does not get indexed and all
+  /// actions taken while offline need to be replayed with each additional
+  /// query, leading to seemingly exponential wait times for simple actions.
+  static bool isOnline = true;
+
   /// Initializes Firefuel with instance of [FirebaseFirestore]
   ///
   /// This method must be called before any other method of Firefuel
@@ -24,13 +50,18 @@ class Firefuel {
   /// ---
   ///
   /// [env] is optional and will be prepended to all collection names
+  /// [isOnline] is optional and will default to `true`. This option
+  /// allows for you to define the initial state of your app. Afterwards use
+  /// `Firefuel.isOnline = true/false` to update the value.
   ///
   /// Useful for separating collections between app flavors / environments
   static void initialize(
     FirebaseFirestore firestore, {
     String? env,
+    bool isOnline = true,
   }) {
     _firestore = firestore;
     _env = env;
+    Firefuel.isOnline = isOnline;
   }
 }
