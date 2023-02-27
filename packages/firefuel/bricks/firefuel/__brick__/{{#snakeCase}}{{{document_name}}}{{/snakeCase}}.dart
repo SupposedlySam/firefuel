b1787c4b26ac18a84a56dd42addfa36d7398be53
Jonah Walker <supposedlysam@gmail.com>
2023-02-27 11:09:19 -0700

    feat: update brick templates with generated ones

--- packages/firefuel/bricks/firefuel/__brick__/{{#snakeCase}}{{{document_name}}}{{/snakeCase}}.dart
+++ packages/firefuel/bricks/firefuel/__brick__/{{#snakeCase}}{{{document_name}}}{{/snakeCase}}.dart
@@ -1,9 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:firefuel/firefuel.dart';
 
-/// The model representing a {{document_name.pascalCase()}} Document in the
-/// {{document_name.pascalCase()}} Collection.
-class {{document_name.pascalCase()}} extends Serializable with EquatableMixin {
+/// The model representing a {{#pascalCase}}{{{document_name}}}{{/pascalCase}} Document in the
+/// {{#pascalCase}}{{{document_name}}}{{/pascalCase}} Collection.
+class {{#pascalCase}}{{{document_name}}}{{/pascalCase}} extends Serializable with EquatableMixin {
   const User({
     required this.docId,
     required this.username,
